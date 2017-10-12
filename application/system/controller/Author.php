a2f71991cfddbf3e3dff1f9927d7241234043b30
seek-concert <1105420247@qq.com>
2017-10-12 15:59:29 +0800

    张_作者管理

---
+++ application/system/controller/Author.php
@@ -0,0 +1,20 @@
+<?php
+/* |------------------------------------------------------
+ * | 作者管理
+ * |------------------------------------------------------
+ * | 列表
+ * | 添加
+ * | 修改
+ * | 删除
+ * */
+namespace app\system\controller;
+use think\Controller;
+
+class Author extends Controller
+{
+    /* ========== 列表 ========== */
+    public function index()
+    {
+        return view();
+    }
+}
