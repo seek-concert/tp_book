1a5eb591be48e410395397151448169ebc6f4861
bulian <452853270@qq.com>
2017-10-13 20:21:56 +0800

    罗_功能与菜单 基本完成

--- application/system/controller/Menu.php
+++ application/system/controller/Menu.php
@@ -4,6 +4,7 @@
  * |------------------------------------------------------
  * | 初始化操作
  * | 列表
+ * | 列表全部
  * | 添加
  * | 详情
  * | 修改
@@ -17,6 +18,7 @@
 namespace app\system\controller;
 
 use \app\system\model\Menus;
+use think\Db;
 
 class Menu extends Auth
 {
@@ -30,7 +32,7 @@ class Menu extends Auth
     /* ========== 列表 ========== */
     public function index()
     {
-        $menus=Menus::field(['id','parent_id','name','icon','sort','url','display','status'])->where('status',1)->order('sort asc')->select();
+        $menus=Menus::field(['id','parent_id','name','icon','sort','url','display','status'])->order('sort asc')->select();
         $table_menus='';
         if($menus){
             $array=[];
@@ -39,7 +41,7 @@ class Menu extends Auth
                 $menu->status=$menu->status($menu->status);
                 $menu->add_url=url('add',['id'=>$menu->id]);
                 $menu->detail_url=url('detail',['id'=>$menu->id]);
-                $menu->delete_url=url('delete',['id'=>$menu->id]);
+                $menu->delete_url=url('delete',['ids'=>$menu->id]);
                 $array[]=$menu;
             }
             $str = "
@@ -47,7 +49,7 @@ class Menu extends Auth
                         <td>
                             <input class='va_m' type='checkbox' name='ids[]' value='\$id' onclick='checkBoxOp(this)' id='check-\$id'/>
                         </td>
-                        <td><input style='width: 50px;' type='text' name='sort[\$id]' value='\$sort' id='input-\$id' data-id='\$id'></td>
+                        <td><input style='width: 50px;' type='text' name='sorts[\$id]' value='\$sort' id='input-\$id' data-id='\$id'></td>
                         <td>\$id</td>
                         <td>\$space \$icon \$name</td>
                         <td>\$url</td>
@@ -62,6 +64,70 @@ class Menu extends Auth
             $table_menus=get_tree($array,$str,0,1,['&nbsp;&nbsp;┃┅','&nbsp;&nbsp;┣┅','&nbsp;&nbsp;┗┅'],'&nbsp;&nbsp;');
         }
         return view('index',['table_menus'=>$table_menus]);
+    }
+
+    /* ========== 列表全部 ========== */
+    public function all(){
+        /* ********** 查询条件 ********** */
+        $datas=[];
+        $where=[];
+        $field=['id','name','parent_id','icon','url','display','status','sort','deleted_at'];
+        /* ++++++++++ 菜单名称 ++++++++++ */
+        $name=trim(request()->param('name'));
+        if($name){
+            $where['name']=['like','%'.$name.'%'];
+            $datas['name']=$name;
+        }
+        /* ++++++++++ 路由地址 ++++++++++ */
+        $url=trim(request()->param('url'));
+        if($url){
+            $where['url']=['like','%'.$url.'%'];
+            $datas['url']=$url;
+        }
+        /* ++++++++++ 显示状态 ++++++++++ */
+        $display=request()->param('display');
+        if(is_numeric($display) && in_array($display,[0,1])){
+            $where['display']=$display;
+            $datas['display']=$display;
+        }
+        /* ++++++++++ 使用状态 ++++++++++ */
+        $status=request()->param('status');
+        if(is_numeric($status) && in_array($status,[0,1])){
+            $where['status']=$status;
+            $datas['status']=$status;
+        }
+        /* ++++++++++ 排序 ++++++++++ */
+        $ordername=request()->param('ordername');
+        $ordername=$ordername?$ordername:'sort';
+        $datas['ordername']=$ordername;
+        $orderby=request()->param('orderby');
+        $orderby=$orderby?$orderby:'asc';
+        $datas['orderby']=$orderby;
+        /* ++++++++++ 每页条数 ++++++++++ */
+        $nums=[config('paginate.list_rows'),30,50,100,200,500];
+        sort($nums);
+        $datas['nums']=$nums;
+        $display_num=request()->param('display_num');
+        $display_num=$display_num?$display_num:config('paginate.list_rows');
+        $datas['display_num']=$display_num;
+        /* ++++++++++ 是否删除 ++++++++++ */
+        $deleted=request()->param('deleted');
+        if(is_numeric($deleted) && in_array($deleted,[0,1])){
+            $datas['deleted']=$deleted;
+            if($deleted==1){
+                $menus=Menus::onlyTrashed()->where($where)->field($field)->order([$ordername=>$orderby])->paginate($display_num);
+            }else{
+                $menus=Menus::where($where)->field($field)->order([$ordername=>$orderby])->paginate($display_num);
+            }
+        }else{
+            $menus=Menus::withTrashed()->where($where)->field($field)->order([$ordername=>$orderby])->paginate($display_num);
+        }
+
+        $datas['menus']=$menus;
+
+        $this->assign($datas);
+
+        return view();
     }
 
     /* ========== 添加 ========== */
@@ -164,39 +230,157 @@ class Menu extends Auth
         $datas=array_merge(input(),$other_datas);
         $menu_model->isUpdate(true)->save($datas);
         if($menu_model !== false){
-            return $this->success('保存成功','');
+            return $this->success('修改成功','');
         }else{
-            return $this->error('保存失败');
+            return $this->error('修改失败');
         }
     }
 
     /* ========== 排序 ========== */
     public function sort(){
-
+        $inputs=input();
+        $ids=isset($inputs['ids'])?$inputs['ids']:'';
+        $sorts=$inputs['sorts'];
+        if(empty($ids)){
+            return $this->error('至少选择一项');
+        }
+        $datas=[];
+        $i=0;
+        $time=time();
+        foreach ($ids as $id){
+            $datas[$i]['id']=$id;
+            $datas[$i]['sort']=(int)$sorts[$id];
+            $datas[$i]['updated_at']=$time;
+            $i++;
+        }
+        $sqls=batch_update_sql('menu',['id','sort','updated_at'],$datas,['sort','updated_at'],'id');
+        $res=false;
+        if($sqls){
+            foreach ($sqls as $sql){
+                $res=db()->execute($sql);
+            }
+        }
+        if($res){
+            return $this->success('修改成功','');
+        }else{
+            return $this->error('修改失败');
+        }
     }
 
     /* ========== 显示状态 ========== */
     public function show(){
+        $inputs=input();
+        $ids=isset($inputs['ids'])?$inputs['ids']:'';
+        $display=$inputs['display'];
 
+        if(empty($ids)){
+            return $this->error('至少选择一项');
+        }
+        if(!in_array($display,[0,1])){
+            return $this->error('错误操作');
+        }
+        $res=Menus::withTrashed()->whereIn('id',$ids)->update(['display'=>$display,'updated_at'=>time()]);
+        if($res){
+            return $this->success('修改成功','');
+        }else{
+            return $this->error('修改失败');
+        }
     }
 
     /* ========== 状态 ========== */
     public function status(){
+        $inputs=input();
+        $ids=isset($inputs['ids'])?$inputs['ids']:'';
+        $status=$inputs['status'];
 
+        if(empty($ids)){
+            return $this->error('至少选择一项');
+        }
+        if(!in_array($status,[0,1])){
+            return $this->error('错误操作');
+        }
+        $res=Menus::withTrashed()->whereIn('id',$ids)->update(['status'=>$status,'updated_at'=>time()]);
+        if($res){
+            return $this->success('修改成功','');
+        }else{
+            return $this->error('修改失败');
+        }
     }
 
     /* ========== 删除 ========== */
     public function delete(){
+        $inputs=input();
+        $ids=isset($inputs['ids'])?$inputs['ids']:'';
 
+        if(empty($ids)){
+            return $this->error('至少选择一项');
+        }
+        if(is_array($ids)){
+            $fail_ids=[];
+            $del_ids[]='0';
+            foreach ($ids as $id){
+                $count=Menus::field('id')->where('parent_id',$id)->count();
+                if($count){
+                    $fail_ids[]=$id;
+                }else{
+                    $del_ids[]=$id;
+                }
+            }
+            $res=Menus::destroy($del_ids);
+            $fail_num=count($fail_ids);
+            if($res){
+                if($fail_num){
+                    return $this->success('部分删除成功！部分存在子菜单，请先删除其全部子菜单后重试！','');
+                }else{
+                    return $this->success('删除成功','');
+                }
+            }else{
+                return $this->error('删除失败');
+            }
+        }else{
+            $count=Menus::field('id')->where('parent_id',$ids)->count();
+            if($count){
+                return $this->error('其下存在子菜单，请先删除全部子菜单后重试！');
+            }
+            $res=Menus::destroy($ids);
+            if($res){
+                return $this->success('删除成功','');
+            }else{
+                return $this->error('删除失败');
+            }
+        }
     }
 
     /* ========== 恢复 ========== */
     public function restore(){
+        $inputs=input();
+        $ids=isset($inputs['ids'])?$inputs['ids']:'';
 
+        if(empty($ids)){
+            return $this->error('至少选择一项');
+        }
+
+        $res=Db::table('menu')->whereIn('id',$ids)->where('`deleted_at` is not null')->update(['deleted_at'=>null,'updated_at'=>time()]);
+        if($res){
+            return $this->success('恢复成功','');
+        }else{
+            return $this->error('恢复失败');
+        }
     }
 
     /* ========== 销毁 ========== */
-    public function destory(){
+    public function destroy(){
+        $inputs=input();
+        $ids=isset($inputs['ids'])?$inputs['ids']:'';
 
+        if(empty($ids)){
+            return $this->error('至少选择一项');
+        }
+        $res=Menus::onlyTrashed()->whereIn('id',$ids)->delete(true);
+        if($res){
+            return $this->success('销毁成功','');
+        }else{
+            return $this->error('销毁失败');
+        }
     }
 }
