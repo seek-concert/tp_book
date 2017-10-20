b9c7bbf576ff081a2fd7df6d491883ccf8942dc2
seek-concert <1105420247@qq.com>
2017-10-20 09:37:25 +0800

    张_订单模型状态更改

--- application/system/model/Rechargeorderss.php
+++ application/system/model/Rechargeorderss.php
@@ -17,7 +17,12 @@ class Rechargeorderss extends Model
     public function getTypeAttr($value=null)
     {
         $status = [0=>'书币',1=>'会员'];
-        return $status[$value];
+        if(is_numeric($value) && in_array($value,[0,1])){
+            return $status[$value];
+        }else{
+            return $status;
+        }
+
     }
     /*----- 关联读者 -----*/
     public function Readers()
