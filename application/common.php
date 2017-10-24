cc38d1a4b96dd0a8397344429b65d25687554368
bulian <452853270@qq.com>
2017-10-24 09:02:29 +0800

    罗_常用工具-上传

--- application/common.php
+++ application/common.php
@@ -283,3 +283,32 @@ function https_request($url, $data = null)
     curl_close($curl);
     return $output;
 }
+
+
+/** 排序
+ * @param $a
+ * @param $b
+ * @return int
+ */
+function cmp_func($a, $b) {
+    global $order;
+    if ($a['is_dir'] && !$b['is_dir']) {
+        return -1;
+    } else if (!$a['is_dir'] && $b['is_dir']) {
+        return 1;
+    } else {
+        if ($order == 'size') {
+            if ($a['filesize'] > $b['filesize']) {
+                return 1;
+            } else if ($a['filesize'] < $b['filesize']) {
+                return -1;
+            } else {
+                return 0;
+            }
+        } else if ($order == 'type') {
+            return strcmp($a['filetype'], $b['filetype']);
+        } else {
+            return strcmp($a['filename'], $b['filename']);
+        }
+    }
+}
