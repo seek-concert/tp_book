ef1b5f91446078d23823cb932b76ac42a83d1d58
seek-concert <1105420247@qq.com>
2017-10-25 16:56:30 +0800

    张_个人中心

--- application/index/controller/Mine.php
+++ application/index/controller/Mine.php
@@ -11,6 +11,18 @@ class Mine extends Auth
 {
     /* ============ 个人中心 ============== */
     public function index(){
+//        $reader_id = $this->reader['id'];
+        $reader_id = 10001;
+        $datas['reader_id'] = $reader_id;
+        $reader_name = '觅树知音';
+        $datas['reader_name'] = $reader_name;
+//        $book_money = db('reader')
+//            ->field('book_money')
+//            ->where('id',$reader_id)
+//            ->find();
+        $book_money = 20;
+        $datas['book_money'] = $book_money;
+        $this->assign($datas);
         return view();
     }
 }
