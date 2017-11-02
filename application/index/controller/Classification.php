467757fc337458b64a25b74d5cbec1ed1ec886b5
seek-concert <1105420247@qq.com>
2017-11-02 10:14:29 +0800

    张_分类加载

--- application/index/controller/Classification.php
+++ application/index/controller/Classification.php
@@ -10,6 +10,13 @@ namespace app\index\controller;
 
 class Classification extends Auth
 {
+    public $data_setting;
+    public function _initialize()
+    {
+        parent::_initialize();
+        $this->data_setting=db('data_setting')->field(['ranking_top','ranking_total','pagenum'])->find();
+    }
+
     /* ============ 分类首页 ============== */
     public function index()
     {
@@ -17,43 +24,7 @@ class Classification extends Auth
         $bookcate_model = db('book_cate');
         $bookcate_name = $bookcate_model->field('id,name')->select();
         $datas['cate_name'] = $bookcate_name;
-        /*+++++ 分类查询 +++++*/
-        $where = [];
-        /*----- 分页条件(分页条数) -----*/
-        $page_num = input('page');
-        if($page_num){
-            $display_num = $page_num;
-            $datas['page_num'] = $page_num;
-        }else{
-            $display_num = config('paginate.list_rows');
-            $datas['page_num'] = $page_num;
-        }
-        /*----- 查询条件(分类) -----*/
-        $cate_id = input('cate_id');
-        if(is_numeric($cate_id)){
-            $where['cate_id'] = "$cate_id";
-            $datas['cate_id'] = $cate_id;
-        }
-        /*----- 查询条件(状态) -----*/
-        $status = input('status');
-        if(in_array($status,[0,1])&&is_numeric($status)){
-            $where['status'] = "$status";
-            $datas['status'] = $status;
-        }
-        /*----- 查询条件(类型) -----*/
-        $type = input('type');
-        if(in_array($type,[0,1])&&is_numeric($type)){
-            $where['type'] = "$type";
-            $datas['type'] = $type;
-        }
-        $book_list = model('books')
-                    ->field(['b.id','b.picture','b.title','a.name as author_name','b.summary'])
-                    ->alias('b')
-                    ->join('author a','b.author_id = a.id','left')
-                    ->where($where)
-                    ->where('online',1)
-                    ->paginate($display_num);
-        $datas['book_list'] = $book_list;
+        $datas['pagenum'] = $this->data_setting['pagenum'];
         $this->assign($datas);
         return view();
     }
@@ -61,20 +32,14 @@ class Classification extends Auth
     /* ============ 分类列表页 ============== */
     public function cate_list()
     {
-        /*+++++ 所有分类 +++++*/
-        $bookcate_model = db('book_cate');
-        $bookcate_name = $bookcate_model->field('id,name')->select();
-        $datas['cate_name'] = $bookcate_name;
         /*+++++ 分类查询 +++++*/
         $where = [];
-        /*----- 分页条件(分页条数) -----*/
-        $page_num = input('page');
-        if($page_num){
-            $display_num = $page_num;
-            $datas['page_num'] = $page_num;
+        /*----- 查询条数 -----*/
+        $fenye_conut = input('fenye_conut');
+        if($fenye_conut){
+            $pagenum = $this->data_setting['pagenum']*$fenye_conut;
         }else{
-            $display_num = config('paginate.list_rows');
-            $datas['page_num'] = $page_num;
+            $pagenum = $this->data_setting['pagenum'];
         }
         /*----- 查询条件(分类) -----*/
         $cate_id = input('cate_id');
@@ -99,9 +64,8 @@ class Classification extends Auth
             ->alias('b')
             ->join('author a','b.author_id = a.id','left')
             ->where($where)
-            ->paginate($display_num);
-        $datas['book_list'] = $book_list;
-        $this->assign($datas);
+            ->where('online',1)
+            ->paginate($pagenum);
        if($book_list){
            $this->success('获取成功','',$book_list);
        }else{
